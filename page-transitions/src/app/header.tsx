cc60e26028cfc965b2fc7d1ba5781ad99bb01d84
devHksingh <hksingh.dev@gmail.com>
2024-05-22 22:33:12 +0530

    add contact and about page

--- page-transitions/src/app/header.tsx
+++ page-transitions/src/app/header.tsx
@@ -8,8 +8,8 @@ export default function Header(){
             <nav className="py-6">
                 <ul className="container flex gap-12 items-center justify-center">
                     <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/'>About</Link></li>
-                    <li><Link href='/'>Contact</Link></li>
+                    <li><Link href='/about'>About</Link></li>
+                    <li><Link href='/contact'>Contact</Link></li>
                 </ul>
             </nav>
         </header>
