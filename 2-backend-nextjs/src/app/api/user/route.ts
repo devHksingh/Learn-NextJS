c786543503712d0fe729518b1d4f9e31a946a6df
devHksingh <hksingh.dev@gmail.com>
2024-05-25 06:56:00 +0530

    fetching data from next server

--- 2-backend-nextjs/src/app/api/user/route.ts
+++ 2-backend-nextjs/src/app/api/user/route.ts
@@ -3,6 +3,7 @@ export function GET(){
     // db logic
     return Response.json({
         name:"test",
-        email:"test@test.com"
+        email:"test@test.com",
+        address: { city: 'Delhi', state: 'Delhi', houseNumber: '21' }
     })
 }
--- 2-backend-nextjs/src/app/page.tsx
+++ 2-backend-nextjs/src/app/page.tsx
@@ -3,9 +3,9 @@ import axios from "axios";
 
 async function getUserDetails(){
   await new Promise((r)=>setTimeout(r,5000))
-  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  // const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
   // console.log(response);
-  
+  const response = await axios.get("http://localhost:3000/api/user")
   return response.data
 }
 //async component
