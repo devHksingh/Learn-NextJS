0f11925cbc073c5e98973353a9e90bebee7d167e
devHksingh <hksingh.dev@gmail.com>
2024-05-25 16:16:03 +0530

    fix axios error in comp post error

--- 2-backend-nextjs/src/app/components/SignUp.tsx
+++ 2-backend-nextjs/src/app/components/SignUp.tsx
@@ -24,11 +24,18 @@ export function Signup() {
                             setPassword(e.target.value)
                         }} label="Password" type={"password"} placeholder="123456" />
                         <button
-                        onClick={()=>{
-                            axios.post("http://localhost:3000/user",{
+                        // onClick={()=>{
+                        //     axios.post("http://localhost:3000/user",{
+                        //         username,
+                        //         password
+                        //     })
+                        // }}
+                        onClick={async () => {
+                            const response = await axios.post("http://localhost:3000/api/user", {
                                 username,
                                 password
-                            })
+                            });
+                            
                         }}
                         type="button"
                          className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
