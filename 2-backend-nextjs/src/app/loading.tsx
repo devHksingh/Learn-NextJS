eb6e3ffa1e30903a9d17bab2aabce369d588a68f
devHksingh <hksingh.dev@gmail.com>
2024-05-25 06:08:45 +0530

    add fetch request with loading state

---
+++ 2-backend-nextjs/src/app/loading.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function loader(){
+    return (
+        <div className="text-center flex justify-center items-center h-screen">
+            <h1>loading...</h1>
+        </div>
+    )
+}
---
+++ 2-backend-nextjs/src/app/page.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+
+async function getUserDetails(){
+  await new Promise((r)=>setTimeout(r,5000))
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  // console.log(response);
+  
+  return response.data
+}
+//async component
+export default async function Home() {
+  const userDetails = await getUserDetails()
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="border  text-left flex-col p-4 rounded-lg shadow-sm">
+        <h1 className="">Name: {userDetails?.name}</h1>
+        <p>Email: {userDetails?.email}</p>
+        <span className="">
+          <h4>Address:</h4>
+          <span>HouseNumber {userDetails.address.houseNumber}, City {userDetails.address.city}, State {userDetails.address.city}</span>
+        </span>
+      </div>
+      {/* <p>{userDetails?.address}</p> */}
+    </div>
+  );
+}
+// address: { city: 'Delhi', state: 'Delhi', houseNumber: '21' }
