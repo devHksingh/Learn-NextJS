4213e719cf19d0c8a38f69570b62a10c72c65134
devHksingh <hksingh.dev@gmail.com>
2024-06-08 13:01:01 +0530

    setup next-auth callback route

--- next-auth-v5-beta/src/auth.ts
+++ next-auth-v5-beta/src/auth.ts
@@ -2,7 +2,12 @@ import NextAuth from "next-auth";
 import google from "next-auth/providers/google";
 import github from "next-auth/providers/github";
 
-NextAuth({
+export const {
+    handlers:{GET,POST},
+    auth,
+    signIn,
+    signOut,
+}=NextAuth({
     providers:[
         google({
             clientId:process.env.GOOGLE_CLIENT_ID,
