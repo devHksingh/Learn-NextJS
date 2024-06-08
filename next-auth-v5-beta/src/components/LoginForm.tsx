1d7976dec661960ba2e9b85a4c8d12cf82da09f6
devHksingh <hksingh.dev@gmail.com>
2024-06-08 21:16:34 +0530

    create login form

--- next-auth-v5-beta/src/components/LoginForm.tsx
+++ next-auth-v5-beta/src/components/LoginForm.tsx
@@ -5,7 +5,37 @@ const LoginForm = () => {
   return (
     <div className="flex-col border p-6 rounded-lg gap-4 items-center justify-center">
       <h1 className="text-xl">LoginForm</h1>
+      <form className="flex flex-col gap-2 mt-6">
+        
+          <label className="text-xl font-semibold text-pretty text-gray-400">Email Address:</label>
+          <input
+          className="border border-gray-400 rounded-lg p-2 text-black font-medium"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email address"
+          />
+        
+        
+          <label className="text-xl font-semibold text-pretty text-gray-400">Password:</label>
+          <input
+          className="border border-gray-400 rounded-lg p-2 text-black font-medium"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          />
+
+          <button
+          type="submit"
+          className="m-4 rounded-xl py-2 capitalize bg-orange-400 text-black hover:bg-lime-600"
+          >
+            Ceredentioal login
+          </button>
+        
+      </form>
       <SocialLogin/>
+      
     </div>
   );
 };
