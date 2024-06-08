8a2cdccad49fb53d1d01ec1bfe57fcd2527514b6
devHksingh <hksingh.dev@gmail.com>
2024-06-08 13:09:39 +0530

    integrate with google btn

--- next-auth-v5-beta/src/app/actions/index.ts
+++ next-auth-v5-beta/src/app/actions/index.ts
@@ -1,8 +1,9 @@
 'use server'
-
+import { signIn } from "@/auth";
 
 export async function doSocialLogin(formData:FormData){
    const action =  formData.get('action')
+   await signIn(action as string,{redirectTo:"/home"})
    console.log(action);
    
 }
