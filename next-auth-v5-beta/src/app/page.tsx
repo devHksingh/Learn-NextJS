6a00cfd72b2407f29c293902191e5920ffdb848c
devHksingh <hksingh.dev@gmail.com>
2024-06-08 12:10:10 +0530

    configure google provider

--- next-auth-v5-beta/src/app/page.tsx
+++ next-auth-v5-beta/src/app/page.tsx
@@ -1,10 +1,14 @@
+import LoginForm from "@/components/LoginForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid place-content-center mt-10">
+    <div className="grid place-content-center mt-10 ">
       <div className="flex justify-center items-center h-screen">
-        <h1 >Login</h1>
+        <div className="flex-col">
+          <h1 className="text-center text-4xl mb-4">Login</h1>
+          <LoginForm/>
+        </div>
       </div>
     </div>
   );
--- next-auth-v5-beta/src/components/LoginForm.tsx
+++ next-auth-v5-beta/src/components/LoginForm.tsx
@@ -1,13 +1,15 @@
+import { doSocialLogin } from "@/app/actions";
+
 const LoginForm = () => {
   return (
-    <div>
-      <h1>LoginForm</h1>
-      <form action="">
+    <div className="flex-col border p-6 rounded-lg gap-4 items-center justify-center">
+      <h1 className="text-xl">LoginForm</h1>
+      <form action={doSocialLogin}>
         <button 
         type="submit"
         name="action"
         value= "google"
-        className="border rounded-xl bg-sky-400 text-black px-4 py-1">
+        className="border rounded-xl bg-sky-400 text-black px-4 py-1 mr-4 mt-4">
           Sign In With Google
         </button>
         <button 
