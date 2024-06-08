64e63dbd0ccee30719095972eae721dfc7b7ca39
devHksingh <hksingh.dev@gmail.com>
2024-06-08 19:04:03 +0530

    display login user data and config next-img

--- next-auth-v5-beta/src/app/home/page.tsx
+++ next-auth-v5-beta/src/app/home/page.tsx
@@ -1,8 +1,25 @@
 
+import { auth } from "@/auth"
+import Image from "next/image"
+import { redirect } from "next/navigation"
 
-const HomePage = () => {
+
+const HomePage = async() => {
+    const session = await auth()
+    if(!session?.user) redirect("/")
   return (
-    <div>HomePage</div>
+    <div className="flex flex-col-reverse items-center m-4 p-8">
+        <h1 className="capitalize"> hi{" "}{session?.user.name}</h1>
+        {session?.user.image && session?.user.name && (
+           <Image
+           src={session.user.image}
+           alt={session.user.name}
+           width={72}
+           height={72}
+           className="rounded-full m-2"
+         />
+        )}
+    </div>
   )
 }
 
