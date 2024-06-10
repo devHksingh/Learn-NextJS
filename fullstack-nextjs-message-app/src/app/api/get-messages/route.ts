305d78f070211a2aa1c5f50ad637530597c4021a
devHksingh <hksingh.dev@gmail.com>
2024-06-10 22:51:32 +0530

    add aggregation pipeline

---
+++ fullstack-nextjs-message-app/src/app/api/get-messages/route.ts
@@ -0,0 +1,42 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+
+export async function GET(request:Request) {
+    
+    await dbConnect()
+    const session = await getServerSession(authOptions)
+    const user:User = session?.user as User
+
+    if(!session || !session.user){
+        return Response.json(
+            {
+                success:false,
+                message:"user not authenticated."
+            },
+            {
+                status:401
+            }
+        )
+    }
+
+    // convert stringify userID to mongoose ObjectId
+    const userId = new mongoose.Types.ObjectId(user._id)
+
+    try {
+        const user = await UserModel.aggregate([
+            { $match:{id:userId}},
+            {$unwind: '$messages'},
+            {$sort:{'messages.createdAt':-1}},
+            {$group:{_id:'$_id',messages:{$push:'$messages'}}}
+        ])
+    } catch (error) {
+        
+    }
+
+
+}
