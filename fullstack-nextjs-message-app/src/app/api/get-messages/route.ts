0a699ec101daccd86ce2fa18d32a5d2694b6fa0d
devHksingh <hksingh.dev@gmail.com>
2024-06-10 22:55:09 +0530

    add route for get messages

--- fullstack-nextjs-message-app/src/app/api/get-messages/route.ts
+++ fullstack-nextjs-message-app/src/app/api/get-messages/route.ts
@@ -34,6 +34,26 @@ export async function GET(request:Request) {
             {$sort:{'messages.createdAt':-1}},
             {$group:{_id:'$_id',messages:{$push:'$messages'}}}
         ])
+        if(!user || user.length === 0){
+            return Response.json(
+                {
+                    success:false,
+                    message:"user not found."
+                },
+                {
+                    status:404
+                }
+            )
+        }
+        return Response.json(
+            {
+                success:true,
+                messages:user[0].messages
+            },
+            {
+                status:200
+            }
+        )
     } catch (error) {
         
     }
