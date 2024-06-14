98901cbd68edf0a8fe2e858e8a9a9d1f3209f356
devHksingh <hksingh.dev@gmail.com>
2024-06-14 12:49:27 +0530

    add dashboard page

---
+++ next-auth-v5-beta/src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div className="flex justify-center h-screen ">
+      
+      <div>
+        <h1 className="mt-8 text-4xl">DashboardPage</h1>
+        <p className="mt-12">You are login</p>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardPage
