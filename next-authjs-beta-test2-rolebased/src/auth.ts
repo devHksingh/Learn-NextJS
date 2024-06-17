6047781f7f9c79e97d13c1d776198c977951eacf
devHksingh <hksingh.dev@gmail.com>
2024-06-17 21:25:54 +0530

    config next-authv5

--- next-authjs-beta-test2-rolebased/src/auth.ts
+++ next-authjs-beta-test2-rolebased/src/auth.ts
@@ -1,5 +1,50 @@
 import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import credentialProvider from "next-auth/providers/credentials"
+import User from "@/model/User"
+import bcrypt from "bcryptjs"
+import dbConnect from "./lib/mongoDB-connection";
 
 export const {handlers,signIn,signOut,auth} = NextAuth({
-    providers:[]
+    ...authConfig,
+    providers:[
+        credentialProvider({
+            credentials:{
+                username:{},
+                email:{},
+                password:{}
+            },
+            async authorize(credentials){
+                let user = null
+                await dbConnect()
+                try {
+                    user = await User.findOne({
+                        $or:[
+                            {email:credentials.email},
+                            {username:credentials.username}
+                        ]
+                    })
+                    if(!user){
+                        throw new Error("User not found")
+                    }
+                    if(!user.isVerified){
+                        throw new Error("Kindly verify your account before login")
+                    }
+                    
+                    const isPasswordCorrect =  bcrypt.compare(credentials.password as string,user.password )
+
+                    if(!isPasswordCorrect){
+                        throw new Error("Check your credentials")
+                    }else{
+                        return user
+                    }
+                    
+
+                } catch (error) {
+                    throw new Error("DB error code 500 ");
+                }
+                
+            }
+        })
+    ]
 })
