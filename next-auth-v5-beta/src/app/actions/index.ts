232918de34e62cb4759fa459fa71ed028042b38e
devHksingh <hksingh.dev@gmail.com>
2024-06-18 19:35:32 +0530

    fix typescript issue

--- next-auth-v5-beta/src/app/actions/index.ts
+++ next-auth-v5-beta/src/app/actions/index.ts
@@ -5,7 +5,10 @@ import { signIn, signOut } from "@/auth";
 
 export async function doSocialLogin(formData:FormData) {
     const action = formData.get('action');
-    await signIn(action, { redirectTo: "/home" });
+    if(action && typeof(action)==="string"){
+      await signIn(action, { redirectTo: "/home" });
+  }
+    
 }
 
 export async function doLogout() {
