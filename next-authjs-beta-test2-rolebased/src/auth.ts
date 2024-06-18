bcd06c798c2986ea74425900c2f95d217a4d5989
devHksingh <hksingh.dev@gmail.com>
2024-06-18 18:18:42 +0530

    add credentials provider and callback

--- next-authjs-beta-test2-rolebased/src/auth.ts
+++ next-authjs-beta-test2-rolebased/src/auth.ts
@@ -4,24 +4,28 @@ import credentialProvider from "next-auth/providers/credentials"
 import User from "@/model/User"
 import bcrypt from "bcryptjs"
 import dbConnect from "./lib/mongoDB-connection";
+import { signInSchema } from "./schema/signIn-schema";
 
 export const {handlers,signIn,signOut,auth} = NextAuth({
     ...authConfig,
     providers:[
         credentialProvider({
             credentials:{
-                username:{},
+                // username:{},
                 email:{},
                 password:{}
             },
-            async authorize(credentials){
+            async authorize(credentials:any):Promise<any>{
                 let user = null
                 await dbConnect()
+                console.log("AUTH.TS => credentials :",credentials);
+                
+                const {identifier,password} =  signInSchema.parse(credentials)
                 try {
                     user = await User.findOne({
                         $or:[
-                            {email:credentials.email},
-                            {username:credentials.username}
+                            {email:credentials.identifier.email},
+                            {username:credentials.identifier.username}
                         ]
                     })
                     if(!user){
@@ -41,10 +45,43 @@ export const {handlers,signIn,signOut,auth} = NextAuth({
                     
 
                 } catch (error) {
-                    throw new Error("DB error code 500 ");
+                    throw new Error("Next auth code 500 ");
                 }
                 
             }
         })
-    ]
+    ],
+    callbacks:{
+        async jwt({ token, user }) {
+            // When user is set (during login), add additional info to token
+            if (user) {
+              token.id = user.id;
+              token.name = user.name;
+              token.email = user.email;
+              token.role = user.role;
+              token.isVerified = user.isVerified
+            }
+            return token;
+          },
+        async session({session,token,user}){
+            
+                if(user){
+                    if(token){
+                
+                        console.log("CALLBACK Session details: Auth.ts :=>");
+                        console.log("session.user.emailVerified ",session.user.emailVerified);
+                        console.log("session.user.email:",session.user.email);
+                        console.log("session.user.id :",session.user.id);
+                        console.log("session.user.image :",session.user.image);
+                        console.log("session.user.name :",session.user.name);
+                        session.user._id = token._id as string
+                        session.user.isVerified = token.isVerified as boolean
+                        session.user.role = token.role as string
+                        
+                    }
+                }
+                return session
+            
+        }
+    }
 })
