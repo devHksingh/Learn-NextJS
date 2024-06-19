a5e4e114f2e75d64640edcbe7685b5f8fe1487e3
devHksingh <hksingh.dev@gmail.com>
2024-06-19 22:25:54 +0530

    fix issue in doCredentialLogin

--- next-authjs-beta-test2-rolebased/src/app/actions/index.ts
+++ next-authjs-beta-test2-rolebased/src/app/actions/index.ts
@@ -2,7 +2,7 @@
 
 import { signIn,signOut } from "@/auth";
 
-export async function doSocialLogin(formData:FormData){
+export async function doSocialLogin(formData){
     const action = formData.get('action')
     console.log("Action => index.ts : action",action);
     if(action && typeof(action)==="string"){
@@ -14,3 +14,25 @@ export async function doSocialLogin(formData:FormData){
 export async function doLogout (){
     await signOut({redirectTo:'/login'})
 }
+
+interface FormData {
+    identifier: string;
+    password: string;
+  }
+
+export async function doCredentialLogin(data:FormData) {
+    console.log("formData", data);
+  
+    try {
+      const response = await signIn("credentials", {
+        identifier: data.identifier,
+        password: data.password,
+        redirect: false,
+      });
+      return response;
+    } catch (err) {
+     console.log("actions error");
+     
+      throw err;
+    }
+  }
