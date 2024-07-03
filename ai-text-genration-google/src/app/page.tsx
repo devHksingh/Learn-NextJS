defd12d554118df43f2406f83f74f518d7e56b68
devHksingh <hksingh.dev@gmail.com>
2024-07-03 19:07:23 +0530

    setup

---
+++ ai-text-genration-google/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="mt-12">
+      <h1 className="text-center">AI text genration</h1>
+    </div>
+  );
+}
