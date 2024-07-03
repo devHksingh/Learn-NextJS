a42701a5c5d15097b9ee81a6631fca179c66c3e4
devHksingh <hksingh.dev@gmail.com>
2024-07-03 19:07:39 +0530

    test

--- next-authjs-beta-test2-rolebased/src/components/navBar/NavBar.tsx
+++ next-authjs-beta-test2-rolebased/src/components/navBar/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { Button } from '../ui/button'
 import { useState } from 'react'
 import { usePathname } from 'next/navigation'
-import ThemeSwitcher, { ModeToggle } from '../ModeToggle'
+import  { ModeToggle } from '../ModeToggle'
 
 
 const NavBar = () => {
