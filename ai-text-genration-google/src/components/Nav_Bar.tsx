ce0ac629bfc7211ab9ced598f9f0bcc2d99901bc
devHksingh <hksingh.dev@gmail.com>
2024-07-03 23:03:07 +0530

    add nav bar

--- ai-text-genration-google/src/components/Nav_Bar.tsx
+++ ai-text-genration-google/src/components/Nav_Bar.tsx
@@ -11,13 +11,6 @@ const Nav_Bar = () => {
     const [darkTheme,setDarkTheme] = useState(true)
     const {setTheme,theme} = useTheme()
     console.log("CURRENT THEME outside:",theme);
-    // if(theme === "dark"){
-    //     setDarkTheme(true)
-    //     setTheme("dark")
-    // }else if(theme === "light"){
-    //     setTheme("light")
-    // }
-
     function setUserTheme(){
         if(darkTheme){
             
@@ -39,8 +32,8 @@ const Nav_Bar = () => {
         }
     }
   return (
-    <header className='p-6 mt-2 '>
-        <nav className='flex gap-6'>
+    <header className='p-6 mt-2 mb-2'>
+        <nav className='flex justify-between items-center gap-6'>
             <div>
                 <Link href={'/'}>
                     <Logo/>
@@ -58,7 +51,7 @@ const Nav_Bar = () => {
                     
                     <span className="sr-only">Toggle theme</span>
                 </Button>
-                {/* <button className='border px-4 rounded-xl'>test</button> */}
+                
             </div>
         </nav>
     </header>
@@ -66,51 +59,4 @@ const Nav_Bar = () => {
 }
 
 export default Nav_Bar
-/*
-<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-*/
 
-/*
-"use client"
-
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
-export function ModeToggle() {
-  const { setTheme } = useTheme()
-
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
-}
-
-*/
