11fc0a75e3088ad785208d4309d0a509e5473bc9
devHksingh <hksingh.dev@gmail.com>
2024-07-04 21:31:45 +0530

    add function for fetch

---
+++ ai-text-genration-google/src/lib/fetchAiText.ts
@@ -0,0 +1,63 @@
+'use server'
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai"
+
+
+async function fetchAiText(prompt:string){
+    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_KEY as string)
+    console.log("hi");
+    
+    try {
+        // const model = genAI.getGenerativeModel({model:"gemini-1.0-pro"})
+
+        const generationConfig = {
+            // stopSequences: ["red"],
+            // maxOutputTokens: 400,
+            temperature: 0.9,
+            topP: 0.1,
+            topK: 16,
+          };
+        
+        const safetySettings = [
+            {
+              category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+              threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+            },
+            {
+              category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+              threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            },
+        ];
+
+        // model
+
+        const model = genAI.getGenerativeModel({model:"gemini-1.0-pro" ,generationConfig,safetySettings})
+
+        // streaming with text-only input
+
+        const result = await model.generateContentStream(prompt)
+
+        let text = ""
+
+        for await (const chunk of result.stream){
+            const chunkText = chunk.text()
+            console.log("chunkText :",chunkText);
+            text += chunkText
+        }
+
+        console.log(" complete para : ",text);
+        
+        return text
+        
+        
+    } catch (error) {
+        console.log(" errror occured while genrating text :",error);
+        
+    }
+}
+
+// const fetchAiText = () => {
+  
+// }
+
+export default fetchAiText
