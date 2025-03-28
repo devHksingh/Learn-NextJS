4b0a3a549697c5a10dbf9282f6eb3855c64dfd01
devHksingh <hksingh.dev@gmail.com>
2025-03-28 22:05:59 +0530

    add dispatch function

--- learn-redux/app/(home)/components/product-card.tsx
+++ learn-redux/app/(home)/components/product-card.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useAppDispatch } from '@/lib/hook'
+import { addProduct } from '@/lib/store/features/cart/cartSlice'
 import { Product } from '@/types/productTypes'
 
 interface ProductCardProps {
@@ -5,13 +8,21 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({product}:ProductCardProps) => {
+  const dispatch = useAppDispatch()
+  const handleAddToCart =(id:number)=>{
+    console.log('adding to cart productId',id);
+    dispatch(addProduct({productId:id,quantity:1}))
+  }
   return (
     <div className='p-2 '>
         <h1>{product.title}</h1>
         <p>{product.brand}</p>
         <p>{product.description}</p>
         <p>{product.price}</p>
-        <button className='border px-2 py-1 '>Add to cart</button>
+        {/* TODO Make btn client comp */}
+        <button className='border px-2 py-1 '
+        onClick={()=>handleAddToCart(product.id)}
+        >Add to cart</button>
     </div>
   )
 }
