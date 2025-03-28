18758cc27a484c4bc71c0a853097e959986e1188
devHksingh <hksingh.dev@gmail.com>
2025-03-28 17:28:38 +0530

    add product type

---
+++ learn-redux/types/productTypes.ts
@@ -0,0 +1,13 @@
+export interface Product{
+    id:string,
+    title:string,
+    description:string,
+    category:string,
+    images:[string],
+    tags:[string],
+    brand:string,
+    price:number,
+    discountPercentage:number
+    rating:number
+    stock:number
+  }
