20ebd787e698316bdb0c0869edf52bbd85e1ff69
devHksingh <hksingh.dev@gmail.com>
2025-03-28 22:36:40 +0530

    show cart item length

--- learn-redux/app/StoreProvider.tsx
+++ learn-redux/app/StoreProvider.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { addProduct } from '@/lib/store/features/cart/cartSlice'
+// import { addProduct } from '@/lib/store/features/cart/cartSlice'
 import { AppStore, makeStore } from '@/lib/store/store'
 import React, { ReactNode, useRef } from 'react'
 import { Provider } from 'react-redux'
@@ -10,7 +10,7 @@ const StoreProvider = ({children}:{children:ReactNode}) => {
     // Create the store instance the first time this renders
     storeRef.current = makeStore()
     // initaily add data on creating store
-    storeRef.current.dispatch(addProduct({productId:123,quantity:1}))
+    // storeRef.current.dispatch(addProduct({productId:123,quantity:1}))
   }
   return (
     <Provider store={storeRef.current}>
--- learn-redux/components/custom/header.tsx
+++ learn-redux/components/custom/header.tsx
@@ -1,10 +1,18 @@
+'use client'
+
+import { useAppSelector } from "@/lib/hook"
 
 const Header = () => {
+  const products = useAppSelector(state=>state.cart)
+
+  console.log("header products",products);
+  
   return (
     <header>
         <nav className="p-2 flex gap-2">
             <button className="bg-sky-600 px-2 py-1 rounded hover:bg-sky-800 cursor-pointer">Cart </button>
-            <p>Total items in cart</p>
+            {/* client comp */}
+            <p>Total items in cart {products.length}</p>
         </nav>
     </header>
   )
